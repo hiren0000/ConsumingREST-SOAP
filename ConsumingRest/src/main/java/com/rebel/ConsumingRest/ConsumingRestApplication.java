37d8b90153a367a9b600ae31156dd6b397b19931
Hiren <rebeldevmurari@gmail.com>
2022-12-08 13:08:27 +0000

    Successfully consumed public Rest API

--- ConsumingRest/src/main/java/com/rebel/ConsumingRest/ConsumingRestApplication.java
+++ ConsumingRest/src/main/java/com/rebel/ConsumingRest/ConsumingRestApplication.java
@@ -1,13 +1,42 @@
 package com.rebel.ConsumingRest;
 
+import com.rebel.ConsumingRest.entity.Joke;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ConsumingRestApplication {
 
+	private static  final Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumingRestApplication.class, args);
 	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder)
+	{
+		return builder.build();
+
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate)
+	{
+		return args -> {
+
+			Joke joke = restTemplate.getForObject("https://official-joke-api.appspot.com/random_joke", Joke.class);
+			log.info(joke.toString());
+
+		};
+	}
+
+
 
 }
